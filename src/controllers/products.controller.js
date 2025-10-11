811b5adaa86b56c78ef8817f2a87232423e4a2ce
D.Bazhenov <dmitriy.bazhenov@gmail.com>
2025-10-11 22:15:39 +0200

    Final update

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,7 +2,7 @@ import { getProducts, addProduct } from '../services/products.service.js';
 
 export const getProductsController = async (req, res) => {
   const result = await getProducts();
-  res.status(200).json(result);
+  res.json(result);
 };
 
 export const addProductsController = async (req, res) => {
