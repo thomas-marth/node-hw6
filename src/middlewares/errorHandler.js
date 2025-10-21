45439231d05f53aa02d09ba04fb3ed43512cf490
Dmytro Bazhenov <63171863+thomas-marth@users.noreply.github.com>
2025-10-21 15:24:11 +0200

    Add handling for UniqueConstraintError in error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,9 +5,11 @@ const errorHandler = (error, req, res, next) => {
   if (error instanceof ValidationError) {
     error.status = 400;
   }
+  
   if (error instanceof UniqueConstraintError) {
     error.status = 409;
   }
+  
   const { status = 500, message = 'Server error' } = error;
   res.status(status).json({
     message,
